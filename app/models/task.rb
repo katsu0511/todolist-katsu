403b51cf21e5b4eff238a5e3404e30400b117964
Katsuya <haradakatsuya190511@gmail.com>
2020-06-30 15:48:35 +0700

    add validation to task model

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,4 +1,8 @@
 class Task < ApplicationRecord
+  validates :title, {presence: true, length: {minimum: 3}}
+  validates :title, {length: {maximum: 50}}
+  validates :content, {presence: true, length: {minimum: 10}}
+  validates :content, {length: {maximum: 400}}
   validates :expiration, presence: true
 
   belongs_to :user
