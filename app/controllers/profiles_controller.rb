332b93da0fa0c92eeda0a8c4b551e6fdc35955e4
katsu <haradakatsuya190511@gmail.com>
2020-07-03 13:03:19 +0700

    Profileにアイコンをアップロードできないようにした

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -13,12 +13,6 @@ class ProfilesController < ApplicationController
     @profile = current_user.prepare_profile
     @profile.assign_attributes(nickname: params[:nickname], introduction: params[:introduction], gender: params[:gender])
 
-    if params[:avatar]
-      @profile.image_name = "user_#{current_user.id}.jpg"
-      avatar = params[:avatar]
-      File.binwrite("public/avatars/#{@profile.image_name}", avatar.read)
-    end
-
     if @profile.save
       redirect_to profile_path, notice: 'successfully updated your profile!'
     else
